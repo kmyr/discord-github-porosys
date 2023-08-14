0b3055d15016818749a3a9fd5c93615c6868a3a2
Kamyar Poureatesam <kmyr.rp@gmail.com>
2023-08-14 15:05:53 +0330

    feat: keep sending message until it succuss

--- src/api/PullRequest/services.js
+++ src/api/PullRequest/services.js
@@ -1,6 +1,36 @@
 const axios = require("axios");
 const config = require("../../common/helper/configs/app.config");
 
+const sendDiscordMessage = async (pullRequestStateToProps) => {
+  const branchRegex = [
+    /\/(PS-\d+)\//i,
+    /\/(ps-\d+)\//i,
+    /\/(Ps-\d+)\//i,
+    /\/(pS-\d+)\//i,
+  ];
+
+  let match;
+  let branch;
+  branchRegex.forEach((regex) => {
+    match = pullRequestStateToProps.headBranchRef.match(regex);
+    branch = match && match[1];
+    if (branch) return;
+  });
+
+  return await axios.post(config.discord_webhook, {
+    username: pullRequestStateToProps.senderName,
+    avatar_url: pullRequestStateToProps.senderAvatar,
+    content: `
+  ${branch ? branch.toUpperCase() : ""}
+  > :bust_in_silhouette: **User**: ${pullRequestStateToProps.pullRequestTitle}
+   :herb: **Branch**: ${pullRequestStateToProps.headBranchRef}
+   :deciduous_tree: **Base Branch**:  ${pullRequestStateToProps.baseBranchRef}
+   :open_file_folder: **Repository**: ${pullRequestStateToProps.repositoryName}
+   :link: **Pull Request Link**: ${pullRequestStateToProps.pullRequestLink}
+`,
+  });
+};
+
 const receiveGithubPullRequest = async (payload) => {
   const pullRequestStateToProps = {
     repositoryName: payload.repository.name,
@@ -14,17 +44,11 @@ const receiveGithubPullRequest = async (payload) => {
 
   if (payload.action !== "opened") return;
 
-  await axios.post(config.discord_webhook, {
-    username: pullRequestStateToProps.senderName,
-    avatar_url: pullRequestStateToProps.senderAvatar,
-    content: `
-    > :bust_in_silhouette: **User**: ${pullRequestStateToProps.pullRequestTitle}
-     :herb: **Branch**: ${pullRequestStateToProps.headBranchRef} 
-     :deciduous_tree: **Base Branch**:  ${pullRequestStateToProps.baseBranchRef}
-     :open_file_folder: **Repository**: ${pullRequestStateToProps.repositoryName}
-     :link: **Pull Request Link**: ${pullRequestStateToProps.pullRequestLink}
-`,
-  });
+  let response;
+
+  do {
+    response = await sendDiscordMessage(pullRequestStateToProps);
+  } while (!(response.status === 204));
 
   return pullRequestStateToProps;
 };
